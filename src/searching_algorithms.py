5b5f3d103ee1d3320e38867b300aa30079e69135
ljkeller <kellerjlucas@gmail.com>
2020-01-02 22:15:37 -0600

    working on color gradient. DFS is wrong- needs to be recursively implemented?

--- src/searching_algorithms.py
+++ src/searching_algorithms.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from math import sqrt
 
 CANVAS_HEIGHT = 816  # for even # squares
 CANVAS_WIDTH = 1392  # full
@@ -8,6 +9,7 @@ NUM_ELEMENTS_Y = (CANVAS_HEIGHT // BLOCK_HEIGHT_WIDTH)
 NUM_ELEMENTS = NUM_ELEMENTS_X * NUM_ELEMENTS_Y
 BASE_FILL = 'Silver'
 MAX_COLOR = 16777215
+COLOR_COUNTER = 0
 
 COLORS = ['snow', 'ghost white', 'white smoke', 'gainsboro', 'floral white',
           'old lace',
@@ -135,6 +137,9 @@ COLORS = ['snow', 'ghost white', 'white smoke', 'gainsboro', 'floral white',
           'MediumPurple2',
           'MediumPurple3', 'MediumPurple4', 'thistle1']
 
+color_gradient = ["#E100FF", "#DF01FF", "#DE02FF", "#DD03FF", "#DC04FF",
+              "#DB05FF", "#DA06FF", "#D907FF", "#D708FF", "#D609FF", "#D50BFF", "#D40CFF", "#D30DFF", "#D20EFF", "#D10FFF", "#D010FF", "#CE11FF", "#CD12FF", "#CC13FF", "#CB15FF", "#CA16FF", "#C917FF", "#C818FF", "#C619FF", "#C51AFF", "#C41BFF", "#C31CFF", "#C21DFF", "#C11FFF", "#C020FF", "#BF21FF", "#BD22FF", "#BC23FF", "#BB24FF", "#BA25FF", "#B926FF", "#B827FF", "#B729FF", "#B62AFF", "#B42BFF", "#B32CFF", "#B22DFF", "#B12EFF", "#B02FFF", "#AF30FF", "#AE31FF", "#AC33FF", "#AB34FF", "#AA35FF", "#A936FF", "#A837FF", "#A738FF", "#A639FF", "#A53AFF", "#A33BFF", "#A23DFF", "#A13EFF", "#A03FFF", "#9F40FF", "#9E41FF", "#9D42FF", "#9C43FF", "#9A44FF", "#9945FF", "#9847FF", "#9748FF", "#9649FF", "#954AFF", "#944BFF", "#924CFF", "#914DFF", "#904EFF", "#8F4FFF", "#8E51FF", "#8D52FF", "#8C53FF", "#8B54FF", "#8955FF", "#8856FF", "#8757FF", "#8658FF", "#8559FF", "#845BFF", "#835CFF", "#825DFF", "#805EFF", "#7F5FFF", "#7E60FF", "#7D61FF", "#7C62FF", "#7B63FF", "#7A65FF", "#7866FF", "#7767FF", "#7668FF", "#7569FF", "#746AFF", "#736BFF", "#726CFF", "#716DFF", "#6F6FFF", "#6E70FF", "#6D71FF", "#6C72FF", "#6B73FF", "#6A74FF", "#6975FF", "#6876FF", "#6677FF", "#6579FF", "#647AFF", "#637BFF", "#627CFF", "#617DFF", "#607EFF", "#5E7FFF", "#5D80FF", "#5C81FF", "#5B83FF", "#5A84FF", "#5985FF", "#5886FF", "#5787FF", "#5588FF", "#5489FF", "#538AFF", "#528BFF", "#518DFF", "#508EFF", "#4F8FFF", "#4E90FF", "#4C91FF", "#4B92FF", "#4A93FF", "#4994FF", "#4895FF", "#4797FF", "#4698FF", "#4499FF", "#439AFF", "#429BFF", "#419CFF", "#409DFF", "#3F9EFF", "#3E9FFF", "#3DA1FF", "#3BA2FF", "#3AA3FF", "#39A4FF", "#38A5FF", "#37A6FF", "#36A7FF", "#35A8FF", "#34A9FF", "#32ABFF", "#31ACFF", "#30ADFF", "#2FAEFF", "#2EAFFF", "#2DB0FF", "#2CB1FF", "#2AB2FF", "#29B3FF", "#28B5FF", "#27B6FF", "#26B7FF", "#25B8FF", "#24B9FF", "#23BAFF", "#21BBFF", "#20BCFF", "#1FBDFF", "#1EBFFF", "#1DC0FF", "#1CC1FF", "#1BC2FF", "#1AC3FF", "#18C4FF", "#17C5FF", "#16C6FF", "#15C7FF", "#14C9FF", "#13CAFF", "#12CBFF", "#10CCFF", "#0FCDFF", "#0ECEFF", "#0DCFFF", "#0CD0FF", "#0BD1FF", "#0AD3FF", "#09D4FF", "#07D5FF", "#06D6FF", "#05D7FF", "#04D8FF", "#03D9FF", "#02DAFF", "#01DBFF", "#00DDFF"]
+
 
 def clear_graph(graph, graph_elements):
     for row in graph_elements:
@@ -142,10 +147,20 @@ def clear_graph(graph, graph_elements):
             graph.TKCanvas.itemconfig(val, fill=BASE_FILL)
 
 
-def update_color(graph: sg.Graph, element: int):
+def update_color(graph: sg.Graph, element: int, color_index: int):
     if graph.TKCanvas.itemconfig(element)['fill'][-1] == 'Silver':
-        graph.TKCanvas.itemconfig(element, fill='red')
+        print(graph.TKCanvas.itemconfig(element))
+        graph.TKCanvas.itemconfig(element, fill=color_gradient[color_index])
         window.read(5)
+
+# def update_colors(graph: sg.Graph, elements: list):
+#     for y in range(NUM_ELEMENTS_Y):
+#         for x in range(NUM_ELEMENTS_X):
+#             if marked[y][x] = 1:
+#                 update_list.append((x,y))
+
+
+
 
 def check_end(tup, end):
     if tup == end:
@@ -179,6 +194,7 @@ boxes = [[0 for i in range(NUM_ELEMENTS_X)] for j in range(
 marked = [[0 for i in range(NUM_ELEMENTS_X)] for j in range(
     NUM_ELEMENTS_Y)]
 bfs_queue = []
+update_list = []
 last_mouse = (None, None)
 mouse = (None, None)
 color_iterator = 1
@@ -292,6 +308,7 @@ while True:
                 if check_end((x, y-1), end_point):
                     break
     elif event == 'BFS':
+        color_iterator = 0
         print(f'Start point: {start_point} and '
               f'end point: {end_point}')
         if start_point == (-1, -1) or end_point == (-1, -1):
@@ -299,6 +316,8 @@ while True:
         x, y = start_point[0], start_point[1]
         bfs_queue.append((x,y))
         while len(bfs_queue) > 0:
+            color_iterator = int(sqrt((start_point[0] - x)**2 + (
+                    start_point[1] - y) ** 2))
             check_end((x,y), end_point)
             x, y = bfs_queue.pop(0)
 
@@ -310,7 +329,7 @@ while True:
             if right_exists and marked[y][x + 1] == 0:
                 right = boxes[y][x + 1]
                 bfs_queue.append((x+1, y))
-                update_color(graph, right)
+                update_color(graph, right, color_iterator)
                 marked[y][x+1] = 1
                 if check_end((x+1, y), end_point):
                     break
@@ -318,49 +337,49 @@ while True:
             if above_exists and right_exists and marked[y + 1][x + 1] == 0:
                 top_right = boxes[y + 1][x + 1]
                 bfs_queue.append((x+1, y+1))
-                update_color(graph, top_right)
+                update_color(graph, top_right, color_iterator)
                 marked[y+1][x+1] = 1
                 if check_end((x+1, y+1), end_point):
                     break
             if below_exists and right_exists and marked[y - 1][x + 1] == 0:
                 bot_right = boxes[y - 1][x + 1]
                 bfs_queue.append((x+1, y-1))
-                update_color(graph, bot_right)
+                update_color(graph, bot_right, color_iterator)
                 marked[y-1][x+1] = 1
                 if check_end((x+1, y-1), end_point):
                     break
             if above_exists and marked[y + 1][x] == 0:
                 top = boxes[y + 1][x]
                 bfs_queue.append((x, y+1))
-                update_color(graph, top)
+                update_color(graph, top, color_iterator)
                 marked[y+1][x] = 1
                 if check_end((x, y+1), end_point):
                     break
             if left_exists and marked[y][x - 1] == 0:
                 left = boxes[y][x - 1]
                 bfs_queue.append((x-1, y))
-                update_color(graph, left)
+                update_color(graph, left, color_iterator)
                 marked[y][x-1] = 1
                 if check_end((x-1, y), end_point):
                     break
             if above_exists and left_exists and marked[y + 1][x - 1] == 0:
                 top_left = boxes[y + 1][x - 1]
                 bfs_queue.append((x-1, y+1))
-                update_color(graph, top_left)
+                update_color(graph, top_left, color_iterator)
                 marked[y+1][x-1] = 1
                 if check_end((x-1, y+1), end_point):
                     break
             if below_exists and left_exists and marked[y - 1][x - 1]:
                 bot_left = boxes[y - 1][x - 1]
                 bfs_queue.append((x-1, y-1))
-                update_color(graph, bot_left)
+                update_color(graph, bot_left, color_iterator)
                 marked[y-1][x-1] = 1
                 if check_end((x-1, y-1), end_point):
                     break
             if below_exists and marked[y - 1][x] == 0:
                 bot = boxes[y - 1][x]
                 bfs_queue.append((x, y-1))
-                update_color(graph, bot)
+                update_color(graph, bot, color_iterator)
                 marked[y-1][x] = 1
                 if check_end((x, y-1), end_point):
                     break
@@ -376,7 +395,7 @@ while True:
 
         print('It happened at: ', mouse)
         x, y = mouse[0] // BLOCK_HEIGHT_WIDTH, mouse[1] // BLOCK_HEIGHT_WIDTH
-        graph.TKCanvas.itemconfig(boxes[y][x], fill='gold')
+        graph.TKCanvas.itemconfig(boxes[y][x], fill='white')
         print(graph.TKCanvas.itemconfig(boxes[y][x])['fill'][-1])  # box color
         marked[y][x] = 1
 
